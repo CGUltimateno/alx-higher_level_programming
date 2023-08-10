e3fd01cb0aa37ff78a57c2a56183ba202ae9ec8c
CGUltimateno <Mohameddeaf65@gmail.com>
2023-08-11 01:38:14 +0300

    5.1

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 import hidden_4
 
+
+def discovery():
+    n = dir(hidden_4)
+    for i in n:
+        if i[:2] != '__':
+            print("{:s}".format(i))
+
 if __name__ == "__main__":
-    for function in dir(hidden_4):
-        if function[0] != "_":
-            print(function)
+    discovery()
