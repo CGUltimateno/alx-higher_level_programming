03380e85ec1fba335db87479ece75f0d8ee357ca
CGUltimateno <Mohameddeaf65@gmail.com>
2023-08-10 23:59:02 +0300

    2 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,11 +3,11 @@ from sys import argv
 
 if __name__ == "__main__":
     i = 1
-    res = 0
-    args = len(argv) - 1
+    result = 0
+    arguments = len(argv) - 1
 
-    if(args != 0):
-        while(i <= args):
-            res += int(argv[i])
+    if (arguments != 0):
+        while (i <= arguments):
+            result += int(argv[i])
             i += 1
-            print(res)
+    print(result)
