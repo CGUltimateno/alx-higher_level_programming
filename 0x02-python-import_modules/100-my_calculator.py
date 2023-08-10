2d613740e9696716e959d24a71efdb5d029bfe36
CGUltimateno <Mohameddeaf65@gmail.com>
2023-08-11 01:25:55 +0300

    100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,16 +2,16 @@
 from sys import argv
 from calculator_1 import add, sub, mul, div
 
-if(__name__ == "__main__"):
+if __name__ == "__main__":
     argc = len(argv)
     res = 0
     operators = ["+", "-", "*", "/"]
 
-    if(argc != 4):
+    if argc != 4:
         print(f"Usage: {argv[0]} <a> <operator> <b>")
         exit(1)
 
-    if(argv[2] not in operators):
+    if argv[2] not in operators:
         print("Unknown operator. Available operators: +, -, *, and /")
         exit(1)
 
@@ -19,11 +19,11 @@ if(__name__ == "__main__"):
     operator = argv[2]
     num2 = int(argv[3])
 
-    if(operator == "+"):
+    if operator == "+":
         res = add(num1, num2)
-    elif (operator == "-"):
+    elif operator == "-":
         res = sub(num1, num2)
-    elif (operator == "*"):
+    elif operator == "*":
         res = mul(num1, num2)
     else:
         res = div(num1, num2)
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ import hidden_4
 
 if __name__ == "__main__":
     for function in dir(hidden_4):
-        if (function[0] != "_"):
+        if function[0] != "_":
             print(function)
