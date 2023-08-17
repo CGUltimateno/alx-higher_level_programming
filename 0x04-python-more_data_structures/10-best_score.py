072925ef0ebc99713945fac882734f0f43c76aa0
CGUltimateno <Mohameddeaf65@gmail.com>
2023-08-17 17:58:41 +0300

    10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+    if a_dictionary is None or a_dictionary == {}:
         return None
-
-    res = list(a_dictionary.keys()[0])
-    larg = a_dictionary[res]
-    for k,v in a_dictionary.items():
-        if v > larg:
-            larg = v
-            res = k
-    return res
+    big = max(a_dictionary.values())
+    for result, value in a_dictionary.items():
+        if value is big:
+            return result
