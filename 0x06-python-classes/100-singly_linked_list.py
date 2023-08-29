d3501f4af276965adcf507da45494001c39137c3
CGUltimateno <Mohameddeaf65@gmail.com>
2023-08-29 20:21:11 +0300

    101

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -59,8 +59,8 @@ class SinglyLinkedList:
             self.__head = new
         else:
             temp = self.__head
-            while(temp.next_node is not None and
-                  temp.next_node.data < value):
+            while (temp.next_node is not None and
+                    temp.next_node.data < value):
                 temp = temp.next_node
             new.next_node = temp.next_node
             temp.next_node = new
