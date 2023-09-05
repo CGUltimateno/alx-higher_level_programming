9d8ea3ae8daf132832f195abfe0a2f92d2c7a623
CGUltimateno <Mohameddeaf65@gmail.com>
2023-09-05 10:18:51 +0300

    0x09-python-everything_is_object is wild

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module that prevents the user from dynamically creating new instance attributes"""
+
+
+class LockedClass:
+    """Class that prevents the user from dynamically creating new instance attributes"""
+    __slots__ = ['first_name']
+
+    def __init__(self):
+        """init"""
+        pass
