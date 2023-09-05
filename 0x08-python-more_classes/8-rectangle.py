46ab3363497afcb38713d3f5ba5a6eaa409607da
CGUltimateno <Mohameddeaf65@gmail.com>
2023-09-05 09:32:40 +0300

    oops all

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -21,10 +21,11 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if not isinstance(self, value):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
+        self.__width = value
 
     @property
     def height(self):
@@ -33,10 +34,11 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if not isinstance(self, value):
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
+        self.__height = value
 
     def area(self):
         """Return area of silly rectangle"""
@@ -61,7 +63,7 @@ class Rectangle:
         if rect_1.area() >= rect_2.area():
             return rect_1
         return rect_2
-    
+
     def __str__(self):
         """Return representation of the silly rectangle"""
 
