05b5c69a507818e93c6fd9fa64e8f05087ea0479
CGUltimateno <Mohameddeaf65@gmail.com>
2023-09-06 10:22:05 +0300

    6

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """unitest for max_integer"""
 import unittest
-max_integer = __import__('6-max_integer.py').max_integer
+max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
