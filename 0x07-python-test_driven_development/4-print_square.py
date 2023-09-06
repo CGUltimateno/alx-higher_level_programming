e4ff9974a354fcf37fc9b5358aac803dcccc8345
CGUltimateno <Mohameddeaf65@gmail.com>
2023-09-06 10:13:08 +0300

    4

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -5,12 +5,11 @@
 def print_square(size):
     """Function that prints a square with #"""
 
-
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
 
-    if not isinstance(size < 0):
+    if size < 0:
         raise ValueError("size must be >= 0")
 
     for i in range(size):
-        print("#" * size)
+        print('#' * size)
