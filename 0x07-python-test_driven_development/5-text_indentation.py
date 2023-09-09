8aab50b06e698a53f13638109e8a20b5990584bd
CGUltimateno <Mohameddeaf65@gmail.com>
2023-09-09 09:42:18 +0300

    5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -12,16 +12,19 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    nl = True
+    x = 0
 
-    for c in text:
-        if nl and c == ' ':
+    while x < len(text) and text[x] == ' ':
+        x += 1
+
+    while x < len(text):
+        print(text[x], end="")
+        if text[x] == "\n" or text[x] in ".?:":
+            if text[x] in ".?:":
+                print("\n")
+            x += 1
+            while x < len(text) and text[x] == ' ':
+                x += 1
             continue
 
-        nl = False
-
-        print(c, end='')
-
-        if c in ".?:":
-            print("\n")
-            nl = True
+        x += 1
