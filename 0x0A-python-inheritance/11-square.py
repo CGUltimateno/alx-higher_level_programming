b990b655bb732dac6a9cfe866f4f856df2ff43ab
CGUltimateno <Mohameddeaf65@gmail.com>
2023-09-12 09:14:38 +0300

    11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-"""Defines a class Square that inherits from Rectangle (10-rectangle.py)"""
+"""
+Defines a class Square that inherits from Rectangle (10-rectangle.py)
+"""
 
 prevsquare = __import__('10-square').Square
 
 
 class Square(prevsquare):
-    """Represent a square."""
+    """
+    Represent a square.
+    """
 
     def __init__(self, size):
-        """Initialize a new square."""
+        """Initialize a constructor of the new square."""
         super().__init__(size)
 
     def __str__(self):
