4729c1b3cbb4c8ba45b335cb9882741bd4e60ffc
CGUltimateno <Mohameddeaf65@gmail.com>
2023-09-19 20:43:57 +0300

    tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -5,43 +5,43 @@ from models.base import Base
 
 
 class TestBase(unittest.TestCase):
-    def setup(self):
-        """ Resets the number of objects """
-        Base._Base__nb_objects = 0
+    def setUp(self):
+        Base.__nb_objects = 0
 
-    def test_id(self):
-        """ tests id """
+    def test_default_id_assignment(self):
         b1 = Base()
-        self.assertEqual(b1.id, 1)
         b2 = Base()
-        self.assertEqual(b2.id, 2)
         b3 = Base()
+
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 2)
         self.assertEqual(b3.id, 3)
 
-    def test_id_set(self):
-        """ tests id """
-        b1 = Base(5)
-        self.assertEqual(b1.id, 5)
-        b2 = Base(12)
-        self.assertEqual(b2.id, 12)
-        b3 = Base(7)
-        self.assertEqual(b3.id, 7)
+    def test_custom_id_assignment(self):
+        b1 = Base(12)
+        b2 = Base(40)
+        b3 = Base(99)
 
-    def test_id_assign(self):
-        """ tests id """
+        self.assertEqual(b1.id, 12)
+        self.assertEqual(b2.id, 40)
+        self.assertEqual(b3.id, 99)
+
+    def test_mixed_id_assignment(self):
         b1 = Base()
-        self.assertEqual(b1.id, 1)
-        b2 = Base(12)
-        self.assertEqual(b2.id, 12)
+        b2 = Base(50)
         b3 = Base()
-        self.assertEqual(b3.id, 2)
 
-    def test_id_increment(self):
-        """ tests id """
+        self.assertEqual(b1.id, 5)
+        self.assertEqual(b2.id, 50)
+        self.assertEqual(b3.id, 6)
+
+    def test_id_increment_after_custom_id(self):
         b1 = Base(100)
-        self.assertEqual(b1.id, 1)
         b2 = Base()
-        self.assertEqual(b2.id, 2)
 
-    if __name__ == '__main__':
-        unittest.main()
+        self.assertEqual(b1.id, 100)
+        self.assertEqual(b2.id, 4)
+
+
+if __name__ == "__main__":
+    unittest.main()
