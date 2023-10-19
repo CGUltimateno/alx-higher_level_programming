a42ff4c82dea6fc878d6b86acf9630a2c226e8eb
CGUltimateno <Mohameddeaf65@gmail.com>
2023-10-19 15:19:05 +0300

    13

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -2,5 +2,5 @@
 SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
 LEFT OUTER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-LEFT OUTER JOIN tv_genres ON tv_show_genres.id = tv_genres.id
+LEFT OUTER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 ORDER BY tv_shows.title, tv_genres.name;
