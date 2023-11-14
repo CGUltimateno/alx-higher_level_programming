fece6560e72b2ea91b4966ab9c2e3239d50ca4b8
CGUltimateno <Mohameddeaf65@gmail.com>
2023-11-13 23:12:50 -0800

    yes

--- 0x12-javascript-warm_up/11-second_biggest.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-let num = process.argv.slice(2);
-if (num.length === 0 || num.length === 1) {
-  console.log(0);
-} else {
-  num = num.map(Number);
-  num.sort(function (a, b) { return b - a; });
-  console.log(num[1]);
-}
