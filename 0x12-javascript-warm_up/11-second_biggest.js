b806cb713ec65e3a0384e6370da118ca9e511a7a
CGUltimateno <Mohameddeaf65@gmail.com>
2023-11-14 09:13:50 +0200

    0x12. JavaScript - Warm up

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+let num = process.argv.slice(2);
+if (num.length === 0 || num.length === 1) {
+    console.log(0);
+} else {
+    num = num.map(Number);
+    num.sort(function (a, b) { return b - a; });
+    console.log(num[1]);
+}
+
