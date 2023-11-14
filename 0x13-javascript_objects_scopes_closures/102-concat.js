da9fac26cefdb0a50c072b30c581fbc1e18c93ee
CGUltimateno <Mohameddeaf65@gmail.com>
2023-11-14 21:48:23 +0200

    extra

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 const filesystem = require('fs').promises;
 
-filesystem.readFile(process.argv[2], 'utf-8')
-.then(contents => { filesystem.writeFile(process.argv[4], contents); })
-.catch(error => console.log(error));
+filesystem.readFile(process.argv[2])
+    .then(contents =>  filesystem.writeFile(process.argv[4], contents) )
+    .catch(error => console.log(error));
 
-filesystem.readFile(process.argv[3], 'utf-8')
-.then(contents => { filesystem.writeFile(process.argv[4], contents, { flag: 'a' }); })
-.catch(error => console.log(error));
+filesystem.readFile(process.argv[3])
+    .then(contents =>  filesystem.writeFile(process.argv[4],
+        contents, { flag: 'a' }) )
+    .catch(error => console.log(error));
