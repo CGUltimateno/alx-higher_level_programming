1042f4500464ba443ffe12279467d660555c78e6
CGUltimateno <Mohameddeaf65@gmail.com>
2023-11-17 20:15:39 +0200

    14

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -6,7 +6,11 @@ from sqlalchemy.orm import relationship
 
 class City(Base):
     """ Defines a class that maps to the table cities in the database """
+
+
     __tablename__ = 'cities'
+
+
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey(states.id), nullable=False)
