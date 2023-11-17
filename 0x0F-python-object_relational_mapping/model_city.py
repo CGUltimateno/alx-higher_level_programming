93096b0a2cb2d31e88d636f3481cb6c2cbcdf4b9
CGUltimateno <Mohameddeaf65@gmail.com>
2023-11-17 20:18:06 +0200

    14

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -15,6 +15,6 @@ class City(Base):
                 primary_key=True)
     name = Column(String(128),
                   nullable=False)
-    state_id = Column(ForeignKey(states.id),
+    state_id = Column(ForeignKey("states.id"),
                       nullable=False)
     state = relationship("State", backref="City")
