62cabea97c383921c68a9f44041e831e8865c80f
CGUltimateno <Mohameddeaf65@gmail.com>
2023-12-01 08:30:38 +0200

    extra

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Bash script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-# shellcheck disable=SC2086
 curl -sx POST -H "Content-type: application/json" -d @2 $1
