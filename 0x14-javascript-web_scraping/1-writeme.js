0083bab9416138bf4ec2022f4675ae4d45b0bbce
CGUltimateno <Mohameddeaf65@gmail.com>
2024-01-04 18:05:15 +0200

    1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+/**
+ * Script that writes a string to a file.
+ *
+ */
+
+const fs = require('fs');
+const { argv } = require('process');
+
+if (argv.length === 4) {
+    fs.writeFile(argv[2], argv[3], 'utf-8', (err, data) => {
+        if (err) {
+        console.log(err);
+        }
+    });
+}
