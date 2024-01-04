531f8ec97ea5d62662a4d5f85ea6c0b04260c5df
CGUltimateno <Mohameddeaf65@gmail.com>
2024-01-04 18:17:55 +0200

    3 4 5 6

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+/**
+ * Script that prints the title of a Star Wars movie where the episode number
+ */
+
+const request = require('request');
+const { argv } = require('process');
+
+if (argv.length === 3) {
+    request(`https://swapi-api.hbtn.io/api/films/${argv[2]}`, (err, res) => {
+        if (err) {
+        console.log(err);
+        } else {
+        console.log(JSON.parse(res.body).title);
+        }
+    });
+}
--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+/**
+ * Script that prints the number of movies where the character “Wedge Antilles”
+ * is present.
+ */
+
+const request = require('request');
+const { argv } = require('process');
+
+if (argv.length === 3) {
+    request(argv[2], (err, res) => {
+        if (err) {
+        console.log(err);
+        } else {
+        console.log(JSON.parse(res.body).results.reduce((count, film) => {
+            return count + film.characters.filter((character) => character.includes('18')).length;
+        }, 0));
+        }
+    });
+}
