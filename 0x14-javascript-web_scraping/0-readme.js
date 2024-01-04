f2ed1976339cf95cc635b36fd2962094b31ce926
CGUltimateno <Mohameddeaf65@gmail.com>
2024-01-04 18:01:56 +0200

    0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+/**
+ * Script that reads and prints the content of a file.
+ */
+
+const fs = require('fs');
+const file = process.argv[2];
+
+fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log(data);
+}
