c9a717a72ad1baae9e891d120efeaac95b5f86e4
CGUltimateno <Mohameddeaf65@gmail.com>
2024-01-10 14:51:49 +0200

    5 6 7 89

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -7,4 +7,4 @@ $.get('https://swapi-api.hbtn.io/api/films/?format=json', (data) => {
     for (const movie of data.results) {
         $('UL#list_movies').append('<li>' + movie.title + '</li>');
     }
-}
+});
